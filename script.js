8d8a63c6927b3c0f18e0f4cd7fa5875860d9de7c
Arief Budiman <mariefbudiman12@gmail.com>
2024-04-20 13:49:08 +0700

    fix: hamburger navbar

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // navbar fixed
 const header = document.querySelector("header");
+const toTop = document.querySelector("#to-top");
 
 window.onscroll = () => {
   const fixedNav = header.offsetTop;
 
-  window.scrollY > fixedNav
-    ? header.classList.add("header-fixed")
-    : header.classList.remove("header-fixed");
+  if (window.scrollY > fixedNav) {
+    header.classList.add("header-fixed");
+    toTop.classList.remove("scale-0");
+    toTop.classList.add("scale-100");
+  } else {
+    header.classList.remove("header-fixed");
+    toTop.classList.remove("scale-100");
+    toTop.classList.add("scale-0");
+  }
 };
 
 // hamburger
@@ -19,4 +26,16 @@ hamburger.addEventListener("click", () => {
     header.classList.toggle("shadow-md");
     nav.classList.toggle("hidden");
   }, 200);
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target != nav && nav.classList.contains("hidden") == false) {
+    if (
+      e.target != hamburger &&
+      hamburger.classList.contains("hamburger-active") != false
+    ) {
+      hamburger.classList.remove("hamburger-active");
+      nav.classList.add("hidden");
+    }
+  }
 });
