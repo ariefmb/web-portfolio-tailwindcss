51edb3c2e58a4081ca2134519ea60a56a968c58e
Arief Budiman <mariefbudiman12@gmail.com>
2025-01-08 20:24:21 +0700

    updated portfolio

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -61,8 +61,47 @@ if (
   (!("theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
-  darkToggle.checked = true
+  darkToggle.checked = true;
 } else {
   document.documentElement.classList.remove("dark");
-  darkToggle.checked = false
+  darkToggle.checked = false;
 }
+
+const toggleProject = document.getElementById("toggle-project");
+const webPro = document.getElementById("web-pro");
+const dataPro = document.getElementById("data-pro");
+const projects = document.getElementById("projects");
+
+if (toggleProject.classList.contains("translate-x-[0px]")) {
+  webPro.style.color = "#082F49";
+}
+
+webPro.addEventListener("click", () => {
+  if (!toggleProject.classList.contains("translate-x-[0px]")) {
+    if (!projects.classList.contains("translate-x-0")) {
+      toggleProject.classList.replace(
+        "translate-x-[117px]",
+        "translate-x-[0px]",
+      );
+      webPro.style.color = "#082F49";
+      dataPro.style.color = "rgb(100 116 139)";
+      projects.classList.replace("translate-x-[-100%]", "translate-x-0");
+      projects.style.height = '1250px'
+    }
+  }
+});
+
+dataPro.addEventListener("click", () => {
+  if (!toggleProject.classList.contains("translate-x-[117px]")) {
+    if (!projects.classList.contains("translate-x-[-100%]")) {
+      toggleProject.classList.replace(
+        "translate-x-[0px]",
+        "translate-x-[117px]",
+      );
+      dataPro.style.color = "#082F49";
+      webPro.style.color = "rgb(100 116 139)";
+      projects.classList.replace("translate-x-0", "translate-x-[-100%]");
+      projects.style.height = '950px'
+    }
+  }
+});
